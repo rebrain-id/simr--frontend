007eaa169cb111150334bf0443ba28a5b787bf20
khoican <ibnukhoirulprasetyo@gmail.com>
2024-08-31 16:18:54 +0700

    feat: create copyright footer

---
+++ src/elements/Footer.jsx
@@ -0,0 +1,19 @@
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => {
+	return (
+		<footer className="absolute bottom-0 mt-20">
+			<p className="text-xs font-light text-light-secondary">
+				<FontAwesomeIcon icon={faCopyright} /> 2024, dibuat dengan{' '}
+				<FontAwesomeIcon icon={faHeart} className="text-danger" /> oleh{' '}
+				<a href="" className="underline">
+					Rebrain Studio
+				</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import Footer from '../elements/Footer';
 
 const MainLayout = () => {
 	return (
-		<main className="min-h-screen w-full flex gap-4 p-2 bg-light-gray">
+		<main className="min-h-screen h-auto w-full flex p-2 bg-light-gray">
 			<Sidebar />
 
-			<aside className="w-full">
+			<aside className="w-full px-10">
 				<Header />
 				<Outlet />
+				<Footer />
 			</aside>
 		</main>
 	);
