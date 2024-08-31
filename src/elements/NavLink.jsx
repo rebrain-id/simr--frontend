4baef36942432fc858abf4ff9219952c55b0e251
khoican <ibnukhoirulprasetyo@gmail.com>
2024-08-31 12:44:20 +0700

    update: add some props to syling NavLink element

--- src/elements/NavLink.jsx
+++ src/elements/NavLink.jsx
@@ -2,12 +2,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
 const NavLink = (props) => {
-	const { icon, title, to } = props;
+	const { icon, title, to, variant, textVariant, iconVariant } = props;
 
 	return (
-		<Link to={to} className="flex gap-3 items-center text-secondary p-2 rounded-md hover:bg-light-primary hover:text-light-white cursor-pointer">
-			<FontAwesomeIcon icon={icon} className="h-5" />
-			<p className="text-md font-semibold">{title}</p>
+		<Link
+			to={to}
+			className={`flex gap-3 items-center text-secondary p-2 rounded-md hover:bg-light-primary hover:text-light-white cursor-pointer ${variant ? variant : ''}`}
+		>
+			<FontAwesomeIcon
+				icon={icon}
+				className={iconVariant ? iconVariant : 'h-5'}
+			/>
+			<p className={textVariant ? textVariant : `text-md font-semibold`}>
+				{title}
+			</p>
 		</Link>
 	);
 };
