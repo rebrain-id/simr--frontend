976deed737a36fd47b7a99d4c6750e76c49fb610
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-01 00:26:05 +0700

    style: change height style on mainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,15 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import Footer from '../elements/Footer';
+import DetailAgendaSidebar from '../components/DetailAgendaSidebar';
 
 const MainLayout = () => {
 	return (
-		<main className="min-h-screen h-auto w-full flex p-2 bg-light-gray">
+		<main className="min-h-screen h-full w-full flex p-2 bg-light-gray">
 			<Sidebar />
 
 			<aside className="w-full px-10">
+				<DetailAgendaSidebar />
 				<Header />
 				<Outlet />
 				<Footer />
