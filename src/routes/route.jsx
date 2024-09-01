443e31cabd021018d63f9ff1920171923a676206
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-01 12:54:17 +0700

    feat: create route for agenda page

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Dashboard from '../pages/Dashboard';
+import Agenda from '../pages/Agenda';
 
 export const router = createBrowserRouter([
 	{
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
 			{
 				index: true,
 				element: <Dashboard />,
+			},
+			{
+				path: 'agenda',
+				element: <Agenda />,
 			},
 		],
 	},
