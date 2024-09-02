2b78f19d8fadeb5557d904dc41912b986ee5f0ac
iQ <estrizqy@gmail.com>
2024-09-03 02:35:57 +0700

    feat: menambahkan route prodi dan dosen

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Dashboard from '../pages/Dashboard';
+import ProgramStudiPage from '../pages/ProgramStudiPage';
+import DosenPage from '../pages/DosenPage';
 
 export const router = createBrowserRouter([
 	{
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
 			{
 				index: true,
 				element: <Dashboard />,
+			},
+			{
+				path: 'prodi',
+				element: <ProgramStudiPage />,
+			},
+			{
+				path: 'dosen',
+				element: <DosenPage />,
 			},
 		],
 	},
