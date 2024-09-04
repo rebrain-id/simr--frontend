d51a6c68f3f43c11aafeb0829dbf76905e797d42
iQ <estrizqy@gmail.com>
2024-09-05 02:20:39 +0700

    fix: memperbaiki styling komposisi modal dan input create dosen & prodi

--- src/components/CreateDosenModal.jsx
+++ src/components/CreateDosenModal.jsx
@@ -1,44 +1,46 @@
 import { useState } from 'react';
 import Button from './../elements/Button';
-import Input from './../elements/forms/FormInput';
+import FormInput from './../elements/forms/FormInput';
 
 const CreateDosenModal = (props) => {
 	const { close } = props;
 	return (
 		<>
 			<div
-				className={`bg-black/25 w-full h-screen fixed top-0 left-0 z-10`}
+				className={`bg-black/25 h-screen fixed top-0 left-0 right-0 bottom-0 z-10`}
 			>
-				<div className="absolute px-56 rounded-lg shadow-lg right-0 top-0 transform -translate-x-1/4 translate-y-1/4 bg-white">
-					<h2 className="text-2xl font-medium pb-8 pt-4">
+				<div className="absolute w-1/2 px-8 rounded-lg shadow-lg right-0 top-0 transform -translate-x-1/2 translate-y-1/4 z-10  bg-white">
+					<h2 className="text-2xl font-medium pb-8 py-2">
 						Tambah Data Dosen
 					</h2>
-					<Input
-						variant="w-full pb-2"
+					<FormInput
+						variant="flex flex-col"
 						type="text"
 						label="Nama Dosen"
 						note="Wajib diisi"
-						labelvariant="text-md"
+						labelvariant="text-md pb-2"
 						inputvariant="text-md"
 						placeholder="Nama Dosen"
 					/>
-					<Input
-						variant="w-full pb-2"
+					<FormInput
+						variant="flex flex-col "
 						type="email"
 						label="E-mail"
-						labelvariant="text-md"
+						note="Wajib diisi"
+						labelvariant="text-md py-2"
 						inputvariant="text-md"
 						placeholder="example@mail.com"
 					/>
-					<Input
-						variant="w-full pb-2"
+					<FormInput
+						variant="flex flex-col "
 						type="number"
 						label="No. Whatsapp"
-						labelvariant="text-md"
+						note="Wajib diisi"
+						labelvariant="text-md py-2"
 						inputvariant="text-md"
 						placeholder="082********"
 					/>
-					<div className="flex items-center justify-end gap-4 py-10">
+					<div className="flex items-center justify-end gap-4 pb-10">
 						<Button
 							text="Simpan"
 							variant="bg-light-primary text-white rounded text-sm hover:bg-primary transition-all ease-in 3s"
--- src/components/CreateProdiModal.jsx
+++ src/components/CreateProdiModal.jsx
@@ -9,38 +9,38 @@ const CreateProdiModal = (props) => {
 			<div
 				className={`bg-black/25 w-full h-screen fixed top-0 left-0 z-10`}
 			>
-				<div className="absolute px-56 rounded-lg shadow-lg right-0 top-0 transform -translate-x-1/4 translate-y-1/4 bg-white">
+				<div className="absolute w-1/2 px-8 rounded-lg shadow-lg right-0 top-0 transform -translate-x-1/2 translate-y-1/4 bg-white">
 					<h2 className="text-2xl font-medium pb-8 pt-4">
 						Tambah Data Program Studi
 					</h2>
 					<Input
-						variant="w-full pb-2"
+						variant="flex flex-col"
 						type="text"
 						label="Nama Program Studi"
 						note="Wajib diisi"
-						labelvariant="text-md"
+						labelvariant="text-md pb-2"
 						inputvariant="text-md"
 						placeholder="Nama Program Studi"
 					/>
 					<Input
-						variant="w-full pb-2"
+						variant="flex flex-col"
 						type="text"
 						label="Username"
 						note="Wajib diisi"
-						labelvariant="text-md"
+						labelvariant="text-md py-2"
 						inputvariant="text-md"
 						placeholder="Username"
 					/>
 					<Input
-						variant="w-full pb-2"
+						variant="flex flex-col"
 						type="password"
 						label="Password"
 						note="Wajib diisi"
-						labelvariant="text-md"
+						labelvariant="text-md py-2"
 						inputvariant="text-md"
 						placeholder="*********"
 					/>
-					<div className="flex items-center justify-end gap-4 py-10">
+					<div className="flex items-center justify-end gap-4 pb-10">
 						<Button
 							text="Simpan"
 							variant="bg-light-primary text-white rounded text-sm hover:bg-primary transition-all ease-in 3s"
