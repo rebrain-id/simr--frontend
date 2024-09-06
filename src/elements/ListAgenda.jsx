2dfe062879e07125feb3470a2120dd7aff9fb11b
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-06 17:46:56 +0700

    style: hidden time information's if date is null

--- src/elements/ListAgenda.jsx
+++ src/elements/ListAgenda.jsx
@@ -20,11 +20,13 @@ const ListAgenda = (props) => {
 			className={`px-5 py-3 border rounded flex items-center cursor-pointer ${isOwner ? 'border-light-primary' : 'border-light-warning'}`}
 			onClick={handleClick}
 		>
-			<div className="pe-3 w-1/2">
+			<div className={`pe-3 ${date ? 'w-1/2' : 'w-2/3'}`}>
 				<h3 className="text-sm font-medium">{title}</h3>
 			</div>
 
-			<div className="w-1/2 flex items-center gap-5">
+			<div
+				className={`flex items-center gap-5 ${date ? 'w-1/2' : 'w-1/3'}`}
+			>
 				<div className="w-3/6 flex items-center gap-2">
 					<FontAwesomeIcon
 						icon={faClockFour}
@@ -36,17 +38,19 @@ const ListAgenda = (props) => {
 					/>
 					<p className="text-xs font-medium">{time}</p>
 				</div>
-				<div className="w-3/6 flex items-center gap-2">
-					<FontAwesomeIcon
-						icon={faCalendarAlt}
-						className={
-							isOwner
-								? 'text-light-primary'
-								: 'text-light-warning'
-						}
-					/>
-					<p className="text-xs font-medium">{date}</p>
-				</div>
+				{date && (
+					<div className="w-3/6 flex items-center gap-2">
+						<FontAwesomeIcon
+							icon={faCalendarAlt}
+							className={
+								isOwner
+									? 'text-light-primary'
+									: 'text-light-warning'
+							}
+						/>
+						<p className="text-xs font-medium">{date}</p>
+					</div>
+				)}
 				<div className="w-1/3 flex items-center gap-2">
 					<FontAwesomeIcon
 						icon={faDoorClosed}
