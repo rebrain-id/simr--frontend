ed2ddda587269e19dd726c5586404e2f40298916
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-07 20:13:43 +0700

    feat: add props to change classname in icons

--- src/elements/calendar/ButtonMenu.jsx
+++ src/elements/calendar/ButtonMenu.jsx
@@ -1,14 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ButtonMenu = (props) => {
-	const { icon, text, variant, onClick } = props;
+	const { icon, text, variant, iconVariant, onClick } = props;
 
 	return (
 		<button
 			onClick={onClick}
 			className={`border-light-primary py-2 px-3 text-sm text-light-primary hover:bg-light-primary hover:text-light-white ${variant}`}
 		>
-			{icon && <FontAwesomeIcon icon={icon} className="mr-2" />}
+			{icon && (
+				<FontAwesomeIcon
+					icon={icon}
+					className={iconVariant ? iconVariant : 'mr-2'}
+				/>
+			)}
 			{text}
 		</button>
 	);
