f546a8c04da543a6b5f31e2c99705017eb2da9b9
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-18 12:04:43 +0700

    refactor: change methode to show detail agenda sidebar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,23 +4,34 @@ import Header from '../components/Header';
 import Footer from '../elements/Footer';
 import DetailAgendaSidebar from '../components/DetailAgendaSidebar';
 import { useDispatch, useSelector } from 'react-redux';
-import { closeDetailAgenda } from '../redux/actions/detailAgendaAction';
+import { useEffect } from 'react';
+import { closeDetailAgenda } from '../redux/actions/agendaAction';
 
 const MainLayout = () => {
-	const { selectedAgenda, showDetailAgenda } = useSelector(
-		(state) => state.datailAgenda,
-	);
+	const { detailAgenda, showSidebar } = useSelector((state) => state.agenda);
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(closeDetailAgenda());
+	}, [dispatch]);
+
+	useEffect(() => {
+		if (showSidebar) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = '';
+		}
+	}, [showSidebar]);
 
 	return (
 		<main className="min-h-screen h-auto w-full flex p-2 bg-light-gray">
 			<Sidebar />
 
 			<aside className="w-full px-10">
-				{showDetailAgenda && (
+				{showSidebar && (
 					<DetailAgendaSidebar
 						onClick={() => dispatch(closeDetailAgenda())}
-						data={selectedAgenda}
+						data={detailAgenda}
 						isShow={true}
 					/>
 				)}
