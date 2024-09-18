45c952a34bfed81d6232ac62ae9bfc40ab21c6a2
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-18 17:23:12 +0700

    feat: add some props

--- src/elements/forms/FormSelect.jsx
+++ src/elements/forms/FormSelect.jsx
@@ -1,10 +1,15 @@
 const FormSelect = (props) => {
-	const { label, note, variant, children } = props;
+	const { label, note, variant, children, value, name, onChange } = props;
 
 	return (
 		<div className={variant ? variant : 'w-80'}>
 			<label className="text-xs font-medium">{label}</label>
-			<select className="w-full border border-light-secondary text-sm rounded py-1 px-3">
+			<select
+				className="w-full border border-light-secondary text-sm rounded py-1 px-3"
+				value={value}
+				name={name}
+				onChange={onChange}
+			>
 				{children}
 			</select>
 
