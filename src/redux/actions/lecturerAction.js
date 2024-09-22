fcd57572ed44186d627a300a2f5e4c1301e68702
iQ <estrizqy@gmail.com>
2024-09-23 00:16:39 +0700

    refactor: refactoring state management dosen & program studi

--- src/redux/actions/lecturerAction.js
+++ src/redux/actions/lecturerAction.js
@@ -1,72 +1,35 @@
 import { getLecturer, postLecturer, updateLecturer, deleteLecturer } from "../../services/lecturer";
 export const FETCH_LECTURER_REQUEST = 'FETCH_LECTURER_REQUEST'
 export const FETCH_LECTURER_SUCCESS = 'FETCH_LECTURER_SUCCESS'
-export const FETCH_LECTURER_FAILURE = 'FETCH_LECTURER_FAILURE'
-export const POST_LECTURER_REQUEST = 'POST_LECTURER_REQUEST'
 export const POST_LECTURER_SUCCESS = 'POST_LECTURER_SUCCESS'
-export const POST_LECTURER_FAILURE = 'POST_LECTURER_FAILURE'
-export const UPDATE_LECTURER_REQUEST = 'UPDATE_LECTURER_REQUEST'
 export const UPDATE_LECTURER_SUCCESS = 'UPDATE_LECTURER_SUCCESS'
-export const UPDATE_LECTURER_FAILURE = 'UPDATE_LECTURER_FAILURE'
-export const DELETE_LECTURER_REQUEST = 'DELETE_LECTURER_REQUEST'
 export const DELETE_LECTURER_SUCCESS = 'DELETE_LECTURER_SUCCESS'
-export const DELETE_LECTURER_FAILURE = 'DELETE_LECTURER_FAILURE'
-
+export const FETCH_LECTURER_FAILURE = 'FETCH_LECTURER_FAILURE'
 
 export const fetchLecturersRequest = () => ({
     type: 'FETCH_LECTURER_REQUEST'
 })
 
-export const fetchLecturersSuccess = (lecturers) => ({
+export const fetchLecturersSuccess = (lecturer) => ({
     type: 'FETCH_LECTURER_SUCCESS',
-    payload: lecturers
+    payload: lecturer
+})
+export const postLecturersSuccess = (lecturer) => ({
+    type: 'POST_LECTURER_SUCCESS',
+    payload: lecturer
+})
+export const updateLecturersSuccess = (lecturer) => ({
+    type: 'UPDATE_LECTURER_SUCCESS',
+    payload: lecturer
+})
+export const deleteLecturersSuccess = (lecturer) => ({
+    type: 'DELETE_LECTURER_SUCCESS',
+    payload: lecturer
 })
 
 export const fetchLecturersFailure = (error) => ({
     type: 'FETCH_LECTURER_FAILURE',
     payload: error
-})
-
-export const postLecturerRequest = () => ({
-	type: 'POST_LECTURER_REQUEST'
-})
-
-export const postLecturerSuccess = (lecturer) => ({
-	type: 'POST_LECTURER_SUCCESS',
-	payload: lecturer
-})
-
-export const postLecturerFailure = (error) => ({
-	type: 'POST_LECTURER_FAILURE',
-	payload: error
-})
-
-export const updateLecturerRequest = () => ({
-	type: 'UPDATE_LECTURER_REQUEST'
-})
-
-export const updateLecturerSuccess = (lecturer) => ({
-	type: 'UPDATE_LECTURER_SUCCESS',
-	payload: lecturer
-})
-
-export const updateLecturerFailure = (error) => ({
-	type: 'UPDATE_LECTURER_FAILURE',
-	payload: error
-})
-
-export const deleteLecturerRequest = () => ({
-	type: 'DELETE_LECTURER_REQUEST'
-})
-
-export const deleteLecturerSuccess = (lecturer) => ({
-	type: 'DELETE_LECTURER_SUCCESS',
-	payload: lecturer
-})
-
-export const deleteLecturerFailure = (error) => ({
-	type: 'DELETE_LECTURER_FAILURE',
-	payload: error
 })
 
 export const fetchLecturers = () => {
@@ -84,39 +47,39 @@ export const fetchLecturers = () => {
 
 export const postLecturerData = (lecturer) => {
 	return async (dispatch) => {
-		dispatch(postLecturerRequest())
+		dispatch(fetchLecturersRequest())
 		try {
 			const response = await postLecturer(lecturer)
 			console.log(response);
-			dispatch(postLecturerSuccess(response))
+			dispatch(postLecturersSuccess(response))
 		} catch (error) {
-			dispatch(postLecturerFailure(error.message))
+			dispatch(fetchLecturersFailure(error.message))
 		}
 	}
 }
 
 export const updateLecturerData = (uuid, lecturer) => {
 	return async (dispatch) => {
-		dispatch(updateLecturerRequest())
+		dispatch(fetchLecturersRequest())
 		try {
 			const response = await updateLecturer(uuid, lecturer)
 			console.log(response);
-			dispatch(updateLecturerSuccess(response))
+			dispatch(updateLecturersSuccess(response))
 		} catch (error) {
-			dispatch(updateLecturerFailure(error.message))
+			dispatch(fetchLecturersFailure(error.message))
 		}
 	}
 }
 
 export const deleteLecturerData = (lecturer) => {
 	return async (dispatch) => {
-		dispatch(deleteLecturerRequest())
+		dispatch(fetchLecturersRequest())
 		try {
 			const response = await deleteLecturer(lecturer)
 			console.log(response);
-			dispatch(deleteLecturerSuccess(response))
+			dispatch(deleteLecturersSuccess(response))
 		} catch (error) {
-			dispatch(deleteLecturerFailure(error.message))
+			dispatch(fetchLecturersFailure(error.message))
 		}
 	}
 }
