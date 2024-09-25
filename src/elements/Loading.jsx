4b06856955ec16b5c0c21489ab92328aebf50ccd
Rizqy <estrizqy@gmail.com>
2024-09-26 06:51:19 +0700

    feat: menambahkan component loading

---
+++ src/elements/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+	return (
+		<div className="w-5 h-5 border-4 border-double rounded-full animate-spin dark:border-black" />
+	);
+};
+
+export default Loading;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,6 +2,7 @@ import Button from '../../elements/Button';
 import FormInput from '../../elements/forms/FormInput';
 import Logo2 from '../../assets/images/logo2.png';
 import Logo3 from '../../assets/images/logo3.jpg';
+import Loading from '../../elements/Loading';
 import { useState } from 'react';
 import { useFormik } from 'formik';
 
@@ -23,74 +24,86 @@ const Login = () => {
 
 	return (
 		<>
-			<div className="pb-2 fixed bottom-0 right-0 transform ">
-				<div className="">
-					<img
-						className="block w-[350px]"
-						src={Logo2}
-						alt="logo-btm"
-					/>
-				</div>
-			</div>
-			<div className="xl:w-1/2 l:w-1/2 md:w-1/3 sm:w-1/3 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white drop-shadow-bottom">
-				<div className="w-full grid grid-cols-2 mx-auto">
-					<div className="w-full py-6 bg-white drop-shadow-md rounded-l hover:drop-shadow-right hover:bg-light-secondary/25 transition-all ease-in duration-300 overflow-hidden">
-						<p className="text-center font-bold text-3xl">SIM-R</p>
-						<p className="pb-6 text-center text-light-secondary text-md">
-							Agenda Management Information System
-						</p>
-
-						<p className="text-center font-semibold text-2xl">
-							Login
-						</p>
-						<form onSubmit={formik.handleSubmit}>
-							<FormInput
-								type="text"
-								name="username"
-								label="Username"
-								labelvariant="pb-2"
-								variant="flex flex-col w-[250px] mx-auto py-6"
-								placeholder="Username"
-							/>
-							<FormInput
-								type="password"
-								name="password"
-								label="Password"
-								labelvariant="pb-2"
-								variant="flex flex-col w-[250px] mx-auto pb-6"
-								placeholder="*********"
-							/>
-							{isSubmit ? (
-								<div className="text-center pt-6">
-									<Button
-										type="submit"
-										text="Login..."
-										variant="text-white px-5 bg-light-secondary"
-										isDisabled={true}
-									/>
-								</div>
-							) : (
-								<div className="text-center pt-6">
-									<Button
-										text="Login"
-										variant="text-white px-5 bg-light-primary hover:bg-primary transition ease-in-out duration-500"
-									/>
-								</div>
-							)}
-						</form>
+			<div className="h-screen bg-light-secondary/30">
+				<div className="pb-2 fixed bottom-0 right-0 transform ">
+					<div className="">
+						<img
+							className="block w-[350px]"
+							src={Logo2}
+							alt="logo-btm"
+						/>
 					</div>
-					<div
-						className="w-full py-6 drop-shadow-bottom rounded-r bg-primary/50"
-						style={{
-							backgroundImage: `url(${Logo3})`,
-							backgroundColor: 'rgb(0, 0, 0, 0.2)',
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-							backgroundRepeat: 'no-repeat',
-							position: 'relative',
-							overflow: 'hidden',
-						}}
-					></div>
+				</div>
+				<div className="xl:w-1/2 l:w-1/2 md:w-1/3 sm:w-1/3 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white drop-shadow-bottom">
+					<div className="w-full grid grid-cols-2 mx-auto">
+						<div className="w-full py-6 bg-white drop-shadow-md rounded-l hover:drop-shadow-right hover:bg-light-secondary/25 transition-all ease-in duration-300 overflow-hidden">
+							<p className="text-center font-bold text-3xl">
+								SIM-R
+							</p>
+							<p className="pb-6 text-center text-light-secondary text-md">
+								Agenda Management Information System
+							</p>
+
+							<p className="text-center font-semibold text-2xl">
+								Login
+							</p>
+							<form onSubmit={formik.handleSubmit}>
+								<FormInput
+									type="text"
+									name="username"
+									label="Username"
+									labelvariant="pb-2"
+									variant="flex flex-col w-[250px] mx-auto py-6"
+									placeholder="Username"
+								/>
+								<FormInput
+									type="password"
+									name="password"
+									label="Password"
+									labelvariant="pb-2"
+									variant="flex flex-col w-[250px] mx-auto pb-6"
+									placeholder="*********"
+								/>
+								{isSubmit ? (
+									<div className="text-center pt-6">
+										<Button
+											type="submit"
+											text={
+												<>
+													<div className="flex items-center gap-2">
+														<span>Login</span>
+														<Loading />
+													</div>
+												</>
+											}
+											variant="text-white px-5 bg-light-secondary/45"
+											isDisabled={true}
+										/>
+									</div>
+								) : (
+									<div className="text-center pt-6">
+										<Button
+											type="submit"
+											text="Login"
+											variant="text-white px-5 bg-light-primary hover:bg-primary transition ease-in-out duration-500"
+										/>
+									</div>
+								)}
+							</form>
+						</div>
+						<div
+							className="w-full py-6 drop-shadow-bottom rounded-r bg-primary/50"
+							style={{
+								backgroundImage: `url(${Logo3})`,
+								backgroundColor: 'rgb(0, 0, 0, 0.2)',
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+								backgroundRepeat: 'no-repeat',
+								position: 'relative',
+								overflow: 'hidden',
+							}}
+						></div>
+					</div>
 				</div>
 			</div>
 		</>
