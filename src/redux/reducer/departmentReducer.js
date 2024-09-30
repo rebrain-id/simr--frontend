677f5a9b689f977b0f5a66e8e8958eba647632a7
khoican <ibnukhoirulprasetyo@gmail.com>
2024-09-30 22:37:04 +0700

    feat: delete useless package

--- src/redux/reducer/departmentReducer.js
+++ src/redux/reducer/departmentReducer.js
@@ -1,68 +1,68 @@
-import { act } from 'react'
 import {
-    FETCH_DEPARTMENT_REQUEST,
-    FETCH_DEPARTMENT_SUCCESS,
-    FETCH_DEPARTMENT_FAILURE,
-    POST_DEPARTMENT_SUCCESS,
-    UPDATE_DEPARTMENT_SUCCESS,
-    DELETE_DEPARTMENT_SUCCESS,
-} from '../actions/departmentAction'
+	FETCH_DEPARTMENT_REQUEST,
+	FETCH_DEPARTMENT_SUCCESS,
+	FETCH_DEPARTMENT_FAILURE,
+	POST_DEPARTMENT_SUCCESS,
+	UPDATE_DEPARTMENT_SUCCESS,
+	DELETE_DEPARTMENT_SUCCESS,
+} from '../actions/departmentAction';
 
 const initialState = {
-    department: [],
-    loading: false,
-    error: 'Program Studi not found',
-    isUpdated: true,
-}
+	department: [],
+	loading: false,
+	error: 'Program Studi not found',
+	isUpdated: true,
+};
 
 const departmentReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_DEPARTMENT_REQUEST:
-            return {
-                ...state,
-                loading: true,
-                error: ''
+	switch (action.type) {
+		case FETCH_DEPARTMENT_REQUEST:
+			return {
+				...state,
+				loading: true,
+				error: '',
+			};
+		case FETCH_DEPARTMENT_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				department: action.payload,
+				error: '',
+			};
+		case POST_DEPARTMENT_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				department: action.payload,
+				error: '',
+				isUpdated: true,
+			};
+		case UPDATE_DEPARTMENT_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				department: action.payload,
+				error: '',
+			};
+		case DELETE_DEPARTMENT_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				department: state.department.filter(
+					(department) => department.uuid !== action.payload,
+				),
+				error: '',
+			};
+		case FETCH_DEPARTMENT_FAILURE:
+			return {
+				...state,
+				loading: false,
+				department: [],
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
 
-            }
-        case FETCH_DEPARTMENT_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                department: action.payload,
-                error: ''
-            }
-        case POST_DEPARTMENT_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                department: action.payload,
-                error: '',
-                isUpdated: true
-            }
-        case UPDATE_DEPARTMENT_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                department: action.payload,
-                error: ''
-            }
-        case DELETE_DEPARTMENT_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                department: state.department.filter((department) => department.uuid !== action.payload),
-                error: ''
-            }
-        case FETCH_DEPARTMENT_FAILURE:
-            return {
-                ...state,
-                loading: false,
-                department: [],
-                error: action.payload
-            }
-        default:
-            return state
-    }
-}
-
-export default departmentReducer
+export default departmentReducer;
