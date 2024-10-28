08bf27dbce08c325dcc88f98a5b4de7f3487b600
khoican <ibnukhoirulprasetyo@gmail.com>
2024-10-28 23:00:34 +0700

    style: change style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,82 +31,90 @@ const Sidebar = () => {
 	};
 
 	return (
-		<nav
-			className={`h-auto bg-light-white rounded-lg px-3 pt-6 drop-shadow-right-bottom ${sidebarFull ? 'w-80' : 'w-24'} transition-all `}
-		>
-			<header
-				className={`flex justify-between items-center mb-28 ${sidebarFull ? '' : 'flex-col justify-center gap-3'}`}
+		<>
+			<nav
+				className={`h-auto bg-light-white rounded-lg px-3 pt-6 drop-shadow-right-bottom ${sidebarFull ? 'w-80' : 'w-24'} transition-all `}
 			>
-				<div className="flex gap-3">
-					<div className="mx-auto my-auto">
-						<img src={logo} alt="" className="w-12 h-auto" />
+				<header
+					className={`flex justify-between items-center mb-28 ${sidebarFull ? '' : 'flex-col justify-center gap-3'}`}
+				>
+					<div className="flex gap-3">
+						<div className="mx-auto my-auto">
+							<img src={logo} alt="" className="w-12 h-auto" />
+						</div>
+
+						{sidebarFull && (
+							<div className="my-auto">
+								<h1 className="text-2xl font-montserrat font-extrabold tracking-widest leading-6">
+									SIM - R
+								</h1>
+								<h2 className="text-xs font-bold tracking-wider">
+									FAKULTAS TEKNIK
+								</h2>
+							</div>
+						)}
 					</div>
 
-					{sidebarFull && (
-						<div className="my-auto">
-							<h1 className="text-2xl font-montserrat font-extrabold tracking-widest leading-6">
-								SIM - R
-							</h1>
-							<h2 className="text-xs font-bold tracking-wider">
-								FAKULTAS TEKNIK
-							</h2>
-						</div>
-					)}
-				</div>
-
-				<div
-					onClick={handleSizeSidebar}
-					className="w-6 h-6 rounded-full border border-primary flex items-center justify-center cursor-pointer"
-				>
 					<div
-						className={`w-4 h-4 rounded-full transition-all hover:bg-light-primary ${sidebarFull ? '' : 'bg-light-primary'}`}
-					></div>
-				</div>
-			</header>
+						onClick={handleSizeSidebar}
+						className="w-6 h-6 rounded-full border border-primary flex items-center justify-center cursor-pointer"
+					>
+						<div
+							className={`w-4 h-4 rounded-full transition-all hover:bg-light-primary ${sidebarFull ? '' : 'bg-light-primary'}`}
+						></div>
+					</div>
+				</header>
 
-			<menu className="flex flex-col gap-2">
-				<NavLink
-					isVisible={sidebarFull}
-					icon={faTableColumns}
-					title="Dashboard"
-					to="/"
-					variant={
-						pathname === '/' ? 'bg-light-primary text-white' : ''
-					}
-				/>
-				<NavLink
-					isVisible={sidebarFull}
-					icon={faCalendarAlt}
-					title="Agenda"
-					to="/agenda?menu=calendar"
-					variant={
-						isActive('/agenda') ? 'bg-light-primary text-white' : ''
-					}
-				/>
-				{role && role === 'FAKULTAS' && (
+				<menu className="flex flex-col gap-2">
 					<NavLink
 						isVisible={sidebarFull}
-						icon={faSitemap}
-						title="Program Studi"
-						to="/prodi"
+						icon={faTableColumns}
+						title="Dashboard"
+						to="/"
 						variant={
-							isActive('/prodi')
+							pathname === '/'
 								? 'bg-light-primary text-white'
 								: ''
 						}
 					/>
-				)}
-				<NavLink
-					isVisible={sidebarFull}
-					icon={faUserAlt}
-					title="Dosen"
-					to="/dosen"
-					variant={
-						isActive('/dosen') ? 'bg-light-primary text-white' : ''
-					}
-				/>
-			</menu>
-		</nav>
+					<NavLink
+						isVisible={sidebarFull}
+						icon={faCalendarAlt}
+						title="Agenda"
+						to="/agenda?menu=calendar"
+						variant={
+							isActive('/agenda')
+								? 'bg-light-primary text-white'
+								: ''
+						}
+					/>
+					{role && role === 'FAKULTAS' && (
+						<NavLink
+							isVisible={sidebarFull}
+							icon={faSitemap}
+							title="Program Studi"
+							to="/prodi"
+							variant={
+								isActive('/prodi')
+									? 'bg-light-primary text-white'
+									: ''
+							}
+						/>
+					)}
+					<NavLink
+						isVisible={sidebarFull}
+						icon={faUserAlt}
+						title="Dosen"
+						to="/dosen"
+						variant={
+							isActive('/dosen')
+								? 'bg-light-primary text-white'
+								: ''
+						}
+					/>
+				</menu>
+			</nav>
+		</>
 	);
 };
 
